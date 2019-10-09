8cf1dbddf06d90ce735ae82cb36595e22c0f904f
Ðтш-ьÑni-marchuk <marchuknikita@gmail.com>
2019-10-09 17:34:14 +0500

    workCommit0910

--- pages/newPage/newPage.vue
+++ pages/newPage/newPage.vue
@@ -1,10 +1,26 @@
 <template>
-    
+    <div id="app">
+        <div class="form-group">
+            <label for="firstName">First name</label>
+            <input type="text" id="firstname" class="form-control" placeholder="firstname" v-model="formData.firstName">
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "newPage"
+        name: "newPage",
+        data() {
+            return {
+                formData: {
+                    firstName: '',
+                    lastName: '',
+                    url: 'http://www',
+                    badge: '',
+                    bookisbn: ''
+                }
+            }
+        }
     }
 </script>
 
